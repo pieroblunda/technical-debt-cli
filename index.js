601666d136cfde4268ee831e9de6174a012e2f1c
Piero <piero.blunda@[D.external@ynap.com>
2020-03-02 14:36:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+let glob = require('glob-gitignore').glob;
+const ignore = require('ignore');
+const chalk = require('chalk');
+
+// Read the .gitignore file
+const gitignoreRules = ignore().add(fs.readFileSync('.gitignore').toString());
+
+function traceToDo(userCustomIgnore){
+  // Glob options
+  let options = {
+    ignore: gitignoreRules.add(userCustomIgnore),
+    extensions: ['md', 'js']
+  };
+
+  // Extensions to search on
+  let ToDoRegEx = /@ToDo:?\s+([\d]+)?([\d\D]*?)$/gmi;
+  let total = 0;
+
+  console.log('\n→ Technical debt trace');
+  console.log('======================');
+  glob(`**/*.{${options.extensions.join(',')}}`, {
+    ignore: options.ignore,
+  }).then( files => {
+    files.forEach((file, i) => {
+      // Read file in asynchronously (non-blocking)
+      let matches = fs.readFileSync(file, 'utf8').match(ToDoRegEx);
+      if(matches){
+        matches.forEach((item, i) => {
+          total++;
+          let msgText = item.replace(ToDoRegEx, '$1$2');
+          let priority = parseInt(item.replace(ToDoRegEx, '$1'));
+          switch (priority) {
+            case 1:
+              console.log(chalk.red(msgText));
+              break;
+            case 2:
+              console.log(chalk.yellow(msgText));
+              break;
+            case 3:
+              console.log(chalk.green(msgText));
+              break;
+            default:
+              console.log(msgText);
+              break;
+          }
+        }); // matches.forEach
+      } // if (matchess)
+    }); // files.forEach
+    console.log(`Total: ${total}`);
+  }); // glob.then
+} // fn traceToDo
+
+//traceToDo(['*.md']);
+traceToDo();
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Technical debit tracer
+
+This programs show in console the technical debit documented inside your code
+
+@ToDo 3 asdasdasdasd
+@ToDo 2 Must ignore the files according the .gitignore file
+@ToDo: 1 Must to match the @_ToDo rule in the jsDoc function (https://jsdoc.app/tags-todo.html)
+@ToDo: 1 Add test
+
+
+Nuevo repo: hacer un ejemplo de repo de GitHub para tomar como modelo para futuros repo
+
+- archivos de configuracino de github
+- modelos de PR y de issue
