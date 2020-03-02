a76fb0187819cb9e1bd2a787195338159f2eac1c
Piero <piero.blunda@[D.external@ynap.com>
2020-03-02 19:34:20 +0100

    Add support for python files

---
+++ fixtures/feature.py
@@ -0,0 +1,2 @@
+# @ToDO: 1 This is a high priority Python todo file
+# @ToDO: 3 This is a low priority Python todo file
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function traceToDo(userCustomIgnore){
   // Glob options
   let options = {
     ignore: gitignoreRules.add(userCustomIgnore),
-    extensions: ['md', 'js', 'css', 'php', 'pug', 'cs', 'html', 'cshtml']
+    extensions: ['md', 'js', 'css', 'php', 'pug', 'cs', 'html', 'cshtml', 'py']
   };
 
   // Extensions to search on
