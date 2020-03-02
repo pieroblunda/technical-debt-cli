221715c4cf1608e22df3e616b28684cfe9b77e44
Piero <piero.blunda@[D.external@ynap.com>
2020-03-02 19:20:24 +0100

    Add support for cs files

---
+++ fixtures/fixture.cs
@@ -0,0 +1,2 @@
+// @ToDo: 1 This is a high priority ToDo in a cs file
+// @ToDo: 3 This is a low priority ToDo in a cs file
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function traceToDo(userCustomIgnore){
   // Glob options
   let options = {
     ignore: gitignoreRules.add(userCustomIgnore),
-    extensions: ['md', 'js', 'css', 'php', 'pug']
+    extensions: ['md', 'js', 'css', 'php', 'pug', 'cs']
   };
 
   // Extensions to search on
