53e679064f6a35fa66376c8f2e5811d5f9731312
Piero <piero.blunda@[D.external@ynap.com>
2020-03-03 13:45:37 +0100

    Refactornig

--- index.js
+++ index.js
@@ -28,26 +28,29 @@ function traceToDo(userCustomIgnore){
     files.forEach((file) => {
       // Read file in asynchronously (non-blocking)
       let matches = fs.readFileSync(file, 'utf8').match(TodoRegEx);
-      if(matches){
-        matches.forEach((item, i) => {
-          let msgText = item.replace(TodoRegEx, '-$2');
-          let priority = parseInt(item.replace(TodoRegEx, '$1'));
-          switch (priority) {
-            case 1: todoList.high.push(msgText); break;
-            case 2: todoList.medium.push(msgText); break;
-            case 3: todoList.low.push(msgText); break;
-            default: todoList.default.push(msgText); break;
-          }
-        }); // matches.forEach
-      } // if (matchess)
+      if (!matches) return false;
+
+      // Iterate over each match in the same file
+      matches.forEach((item, i) => {
+        let msgText = item.replace(TodoRegEx, '-$2');
+        let priority = parseInt(item.replace(TodoRegEx, '$1'));
+        switch (priority) {
+          case 1: todoList.high.push(msgText); break;
+          case 2: todoList.medium.push(msgText); break;
+          case 3: todoList.low.push(msgText); break;
+          default: todoList.default.push(msgText); break;
+        }
+      }); // matches.forEach
+
     }); // files.forEach
 
-    // Print the ordered result 
+    // Print the ordered result
     todoList.high.forEach((item, i) => console.log(chalk.red(item)));
     todoList.medium.forEach((item, i) => console.log(chalk.yellow(item)));
     todoList.low.forEach((item, i) => console.log(chalk.cyan(item)));
     todoList.default.forEach((item, i) => console.log(item));
     console.log(`Total: ${todoList.high.length + todoList.medium.length + todoList.low.length + todoList.default.length}`);
+
   }); // glob.then
 } // fn traceToDo
 
