dadef1a3cd22a45b2e7214a1f34541a8811fa30e
Piero <piero.blunda@[D.external@ynap.com>
2020-03-04 22:28:13 +0100

    Add Test for Python

--- fixtures/feature.py
+++ fixtures/feature.py
@@ -1,3 +1,4 @@
-# @TODO: 1 Python This is a high priority todo file
-# TODO: 2 Python This is a medium priority todo file
-# @TODO: 3 Python This is a low priority todo file
+# @TODO: Python This is an unclassified priority task
+# @TODO: 1 Python This is a high priority task
+# TODO: 2 Python This is a medium priority task
+# @TODO: 3 Python This is a low priority task
